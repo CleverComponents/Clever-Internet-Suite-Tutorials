878ba1e94620b958f340c1b8731f89b388aac3ec
Nikita Fomka <nikitafomka005@yandex.ru>
2019-12-11 22:10:57 +0300

    Add new Project in .NET

---
+++ .net/PingClient/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CleverComponents.InetSuite;
+
+namespace PingClient
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnPing_Click(object sender, EventArgs e)
+		{
+			using (var client = new IcmpConnection())
+			{
+				client.NetworkStream = new NetworkStream();
+				client.TimeOut = 1000;
+				client.Open(HostResolver.GetIPAddress(txtHost.Text));
+
+				txtResult.Text = "";
+
+				for (int i = 0; i < 4; i++)
+				{
+					try
+					{
+						client.SendEchoRequest();
+						var response = client.ReceiveResponse();
+						if (!(response.IcmpPacket is IcmpEchoPacket))
+						{
+							throw new Exception("Invalid ICMP reply");
+						}
+
+						txtResult.Text += String.Format("Reply from {0}: seq={1} received bytes={2} time={3}ms TTL={4}\r\n",
+							txtHost.Text,
+							(response.IcmpPacket as IcmpEchoPacket).SequenceNumber,
+							(response.IcmpPacket as IcmpEchoPacket).Data.Length,
+							response.RoundTripTime,
+							response.IpHeader.Ttl);
+
+						System.Threading.Thread.Sleep(1000);
+					}
+					catch (Exception ex)
+					{
+						  txtResult.Text += " Error: " + ex.Message + "\r\n";
+					}
+				}
+			}
+		}
+	}
+}
---
+++ .net/PingClient/README.md
@@ -0,0 +1,12 @@
+# Ping Client in .NET
+
+<img align="left" src="PingClient.jpg"/>
+
+The PingClient project represents a code example for the [Send SMTP email with Office365 account](https://www.clevercomponents.com/portal/kb/a75/ping-client.aspx) tutorial.   
+
+The sample represents simple ICMP client with Ping functionality.   
+
+The [GitHub/CleverComponents/Clever-Internet-Suite-Tutorials](https://github.com/CleverComponents/Clever-Internet-Suite-Tutorials) repository represents a list of examples, code snippets and demo projects for the [Clever Internet Suite Tutorials](https://www.clevercomponents.com/articles/article035/) article. This list will be periodically updated, new projects will be added.   
+Please stay tuned to new examples and use cases of the [Clever Internet Suite](https://www.clevercomponents.com/products/inetsuite/) library.
+
+**How to compile and run** - see [Lib/ReadMe](./Lib/ReadMe.md)   
